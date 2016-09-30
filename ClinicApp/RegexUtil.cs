3eb1d516101401ee424d977fb814ea84a453b2e1
DUHO <duhowise@gmail.com>
2016-09-30 14:30:11 +0000

    updated DrugsXaml to use new model

---
+++ ClinicApp/RegexUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Input;
+
+namespace ClinicApp
+{
+    public static class RegexUtil
+    {
+        private static void TextValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex("[^a-zA-Z]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private static void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
