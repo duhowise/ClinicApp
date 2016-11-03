c968bb526a160f87b3438148f5d5df422326f7c5
DUHO <duhowise@gmail.com>
2016-11-03 14:52:43 +0000

    Bug Fix for investigation/Signs

--- ClinicApp/Doctor/DocPatientDetailConsultation.xaml.cs
+++ ClinicApp/Doctor/DocPatientDetailConsultation.xaml.cs
@@ -41,7 +41,7 @@ namespace ClinicApp.Doctor
                 PatientPulse.Content = $"Pulse :{_consultation?.Pulse} bpm";
                 tbDiagnosis.Text = _consultation?.Diagnosis;
                 tbPrescriptions.Text = _consultation?.Prescription;
-                tbFindings.Text = _consultation?.Signs;
+                tbFindings.Text = _consultation?.Investigation;
                 PatientWeight.Content = $"Weight :{_consultation?.Weight} kg";
                 PatientBloodPressure.Content = $"Blood pressure : {_consultation?.BloodPressure}";
                 PatientLastVisited.Content = $"Consultation Date: {_consultation?.Date.ToShortDateString()}";
